3cc7fb7af2218a42d61aa9e639f22b0d92c0438b
nathandstory <nathandstory@gmail.com>
2019-09-06 17:52:37 -0500

    implemented voting and help command, Improved readability, offloaded common tasks to their own function.

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 
 //Uses sqlite3 for storing points in a database that can be recalled when the program starts and stops.
 const config = require("./config.json");
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
 const SQLite = require("better-sqlite3");
+const Prompter = require('discordjs-prompter');
 const sql = new SQLite('./scores.sqlite');
+const client = new Discord.Client();
 const master = config.masterID;
 const singPoint = config.pointName;
 const plurPoint = config.pointName + 's';
@@ -24,11 +25,9 @@ client.once('ready', () => {
     sql.pragma("synchronous = 1");
     sql.pragma("journal_mode = wal");
   }
-
   // And then we have two prepared statements to get and set the score data.
   client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
   client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points) VALUES (@id, @user, @guild, @points);");
-
   //Send a message to the general chat that the bot is online.
   try{client.channels.get(config.channelToTalk).send('Bot Online!');}catch(err){console.log("No Channel Set")}
 });
@@ -37,100 +36,137 @@ client.once('ready', () => {
 client.on('message', message => {
 //ignore bots, direct messages, and group messages
 if (message.author.bot || !message.guild) return;
-
-
   //Beginning of score code and commands
   // Initialize ("declare") the points. If we did this in the condition it would not be
   // available later in commands. Because "scopes"! 
   let score;
-  
   if (message.guild) {
     // Try to get the current user's point score. 
     score = client.getScore.get(message.author.id, message.guild.id);
-    
     // If the score doesn't exist (new user), initialize with defaults. 
     if (!score) {
       score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0 };
     }
-
     // Save data to the sqlite table. 
     // This looks super simple because it's calling upon the prepared statement!
     client.setScore.run(score);
   }
   if (message.content.indexOf(config.prefix) !== 0) return;
-
+  //Parse the command
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  //Tell the user how many points they have (command is determined by adding s onto the point name in config)
+
+  //--UPDATE-SCORE-------------------------------------------------------------------------------------------------------------------------------
+  //Updates the score based on the recieved parameters
+  const updateScore = (message, user, value, set = false) => {
+    //Get the current points from the user whos score we are trying to change
+    let userscore = client.getScore.get(user.id, message.guild.id);
+    //Initiate defaults for users that may not be in the databse yet
+    if (!userscore) userscore = { id: `${message.guild.id}-${user.id}`, user: user.id, guild: message.guild.id, points: 0 };
+    //Adjust the score
+    if(set) userscore.points = value;
+    else userscore.points += value;
+    //Set and Save the new score
+    client.setScore.run(userscore);
+    return console.log("Scores have been updated.");
+  }
+
+  //--/STANDING------------------------------------------------------------------------------------------------------------------------------------
+  //Anyone can use this command - 
+  //Tell the user how many points they have.
   if(command === "standing") {
-    if (score.points == 1) return message.reply(`You currently have ${score.points} ${singPoint}.`)
+    if (score.points == 1) return message.reply(`you currently have ${score.points} ${singPoint}.`)
     else return message.reply(`You currently have ${score.points} ${plurPoint}.`)
   }
-  //WIP
-  if(command === "poll"){}
-  //Owner and Master Only - Give points
+
+  //--/VOTE----------------------------------------------------------------------------------------------------------------------------------------
+  //Anyone can make a vote to give or remove points
+  if(command === "vote"){
+    //Get the subcommand (give/remove)
+    const subCommand = args[0];
+    if(!subCommand || !(subCommand === 'give' || subCommand === 'remove')) return message.reply("you must tell me to either 'give' or 'remove' points in a vote.");
+    //get the user
+    const user = message.mentions.users.first() || client.users.get(args[0]);
+    if(!user) return message.reply("you must mention someone or give their ID!");
+    //get the amount of points to add or remove
+    const pointValue = parseInt(args[2], 10);
+    if(!pointValue) return message.reply(`ylease specify an amount of ${plurPoint}.`);
+    //--VOTE-EMBED-HANDLER-----------------------------------------------------------------------------
+    Prompter.vote(message.channel, {
+      question: `Should ${user} ${(subCommand === 'give')?('receive'):('lose')} ${pointValue} ${(pointValue === 1)?(singPoint):(plurPoint)}?`,
+      choices: ['✅', '❌'],
+      timeout: 30000,
+    }).then((response) => {
+      const winner = response.emojis[0];
+      //if the vote passe
+      if(winner.emoji === '✅') 
+      {
+        message.channel.send(`The vote to ${(subCommand === 'give')?('give'):('take away')} ${pointValue} ${(pointValue === 1)?(singPoint):(plurPoint)} ${(subCommand === 'give')?('to'):('from')} ${user} has passed`);
+        if(subCommand === "give") return updateScore(message, user, pointValue);
+        else if(subCommand === "remove") return updateScore(message, user, -pointValue);
+        else console.log("Vote changes failed to update");
+      }
+      //if the vote fails
+      if(winner.emoji === '❌') 
+        message.channel.send(`The vote to ${(subCommand === 'give')?('give'):('take away')} ${pointValue} ${(pointValue === 1)?(singPoint):(plurPoint)} ${(subCommand === 'give')?('to'):('from')} ${user} has failed`);
+    });
+  }
+
+  //--/GIVE-----------------------------------------------------------------------------------------------------------------------------------------
+  //Owner and Master only - Give points
   if(command === "give") {
     // Limited to guild owner or master id
-    if(!((message.author.id === message.guild.owner.id) || (message.author.id === master))) return message.reply("Sorry champ you can't do that!");
-  
+    if(!((message.author.id === message.guild.owner.id) || (message.author.id === master))) return message.reply("sorry champ you can't do that!");
     // Try to get the user from mention. If not found, get the ID given and get a user from that ID. 
     const user = message.mentions.users.first() || client.users.get(args[0]);
-    if(!user) return message.reply("You must mention someone or give their ID!");
-  
+    if(!user) return message.reply("you must mention someone or give their ID!");
     // Read the amount of points to give to the user. 
     const pointsToAdd = parseInt(args[1], 10);
-    if(!pointsToAdd) return message.reply("You didn't tell me how many points to give...");
-  
-    // Get their current points. This can't use `score` because it's not the same user ;)
-    let userscore = client.getScore.get(user.id, message.guild.id);
-    
-    // It's possible to give points to a user we haven't seen, so we need to initiate defaults here too!
-    if (!userscore) {
-      userscore = { id: `${message.guild.id}-${user.id}`, user: user.id, guild: message.guild.id, points: 0 };
-    }
-    
-    // Increment the score. 
-    userscore.points += pointsToAdd;
-  
-    // And we save it!
-    client.setScore.run(userscore);
-    
+    if(!pointsToAdd) return message.reply(`you didn't tell me how many ${plurPoint} to give...`);
+    //update the score
+    updateScore(message, user, pointsToAdd);
     //Test for plural or singular points
     if (pointsToAdd != 1)return message.channel.send(`${user.tag} has received ${pointsToAdd} ${plurPoint}.`);
     else return message.channel.send(`${user.tag} has received ${pointsToAdd} ${singPoint}.`);
   }
   
+  //--/REMOVE-------------------------------------------------------------------------------------------------------------------------------------------
+  //Owner and Master only - Remove Points
   if(command === "remove") {
     // Limited to guild owner and master id.
-    if(!((message.author.id === message.guild.owner.id) || (message.author.id === master))) return message.reply("Sorry champ you can't do that!");
-  
+    if(!((message.author.id === message.guild.owner.id) || (message.author.id === master))) return message.reply("sorry champ you can't do that!");
     // Try to get the user from mention. If not found, get the ID given and get a user from that ID. 
     const user = message.mentions.users.first() || client.users.get(args[0]);
-    if(!user) return message.reply("You must mention someone or give their ID!");
-  
+    if(!user) return message.reply("you must mention someone or give their ID!");
     // Read the amount of points to remove from the user. 
     const pointsToRemove = parseInt(args[1], 10);
-    if(!pointsToRemove) return message.reply("You didn't tell me how many points to remove...");
-  
-    // Get their current points. This can't use `score` because it's not the same user.
-    let userscore = client.getScore.get(user.id, message.guild.id);
-    
-    // It's possible to give points to a user we haven't seen, so we need to initiate defaults here too!
-    if (!userscore) {
-      userscore = { id: `${message.guild.id}-${user.id}`, user: user.id, guild: message.guild.id, points: 0 };
-    }
-    
-    // Increment the score. 
-    userscore.points -= pointsToRemove;
-  
-    // And we save it!
-    client.setScore.run(userscore);
-	
+    if(!pointsToRemove) return message.reply(`you didn't tell me how many ${plurPoint} to remove...`);
+    //update the score
+    updateScore(message, user, -pointsToRemove);
     //Test for plural or singular points
     if (pointsToRemove != 1)return message.channel.send(`${user.tag} has lost ${pointsToRemove} ${plurPoint}.`);
-    else return message.channel.send(`${user.tag} has received ${pointsToRemove} ${singPoint}.`);
+    else return message.channel.send(`${user.tag} has lost ${pointsToRemove} ${singPoint}.`);
   }
-  
+    
+  //--/SET---------------------------------------------------------------------------------------------------------------------------------------------  
+  //Owner and Master Only - Set points (try to avoid using this, its a boring command for corrective purposes only)
+  if(command === "set") {
+    // Limited to guild owner or master id
+    if(!((message.author.id === message.guild.owner.id) || (message.author.id === master))) return message.reply("sorry champ you can't do that!");
+    // Try to get the user from mention. If not found, get the ID given and get a user from that ID. 
+    const user = message.mentions.users.first() || client.users.get(args[0]);
+    if(!user) return message.reply("you must mention someone or give their ID!");
+    // Read the amount of points to give to the user. 
+    const pointsToSet = parseInt(args[1], 10);
+    if(!pointsToSet) return message.reply(`you didn't tell me what to set their ${plurPoint} to...`);
+    //update the score
+    updateScore(message, user, pointsToSet, true);
+    //Test for plural or singular points
+    return message.channel.send(`${user.tag}'s ${plurPoint} has been set to ${pointsToSet}.`);
+  }
+
+  //--/LEADERBOARD--------------------------------------------------------------------------------------------------------------------------------------
+  //Anyone can use the leader board command - shows standings sorted by point values
   if(command === "leaderboard") {
     // Grab the top 10
     const top10 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 10;").all(message.guild.id);
@@ -138,8 +174,7 @@ if (message.author.bot || !message.guild) return;
     //Make it look good
     const embed = new Discord.RichEmbed()
       .setTitle("Leaderboard")
-      .setAuthor(client.user.username, client.user.avatarURL)
-      .setDescription("The top point holders:")
+      .setDescription(`The top ${singPoint} holders:`)
       .setColor(0x00AE86);
 
     for(const data of top10) {
@@ -148,6 +183,22 @@ if (message.author.bot || !message.guild) return;
     return message.channel.send({embed});
   }
 
+  //--/HELP---------------------------------------------------------------------------------------------------------------------------------------------  
+  //Available to anyone - shows all available commands and embeds them to look nice
+  if(command === "help") {
+   const embed = new Discord.RichEmbed()
+   .setTitle("Available Commands:")
+   .setColor(0x00AE86);
+
+   embed.addField("/standing", `Tells you how many ${plurPoint} you currently have.`);
+   embed.addField("/vote", `Allows you to make a vote to give or remove ${plurPoint} from an individual.`);
+   embed.addField("/leaderboard", `Shows the top ${singPoint} holders.`);
+   embed.addField("/give", `Gives an individual ${plurPoint}. Requires permissions.`);
+   embed.addField("/remove", `Takes an individual's ${plurPoint}. Requires permissions.`);
+   embed.addField("/set", `Sets an individual's ${plurPoint}. Requires permissions.`);
+
+   return message.channel.send({embed});
+  }
 })
 
 client.login(config.token)
